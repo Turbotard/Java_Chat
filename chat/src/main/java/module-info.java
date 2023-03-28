f0c08b1935ec309c51dbc18d21c6fb92a4f06adb
Lucas SALAUN <lucas.salaun2@gmail.com>
2023-03-28 14:19:10 +0200

    intellij init

--- chat/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module chat.java {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires javafx.graphics;
-
-    opens chat.java to javafx.fxml;
-    exports chat.java;
-}
---
+++ client/src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module chat.client.client {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens chat.client.client to javafx.fxml;
+    exports chat.client.client;
+}
